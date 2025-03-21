23c5e43135ab31a64b8863ec83ba230a74ceecc9
naveen200497 <naveenacm093@gmail.com>
2025-03-21 11:14:11 +0530

    Update Sample.java

--- JavaD1/src/Sample.java
+++ JavaD1/src/Sample.java
@@ -1,11 +1,11 @@
 public class Sample {
 	public static void main(String[]args) {		
-		System.out.println("ENTER THE NAME:SOWDHANYA LAXNA.V");
-		System.out.println("ENTER THE DOB:17/05/2004");
-		System.out.println("ENTER THE GENDER:FEMALE");
+		System.out.println("ENTER THE NAME:Naveen Kumar.K.M");
+		System.out.println("ENTER THE DOB:20/09/2004");
+		System.out.println("ENTER THE GENDER:MALE");
 		System.out.println("ENTER THE AGE:21");
 		System.out.println("ENTER THE PHONE:9********8");
-		System.out.println("ENTER THE EMAIL:sow@gmail.com");
+		System.out.println("ENTER THE EMAIL:Naveen@gmail.com");
 	}
 
 }
